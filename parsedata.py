1561aa9957a245d5f7cf35fb28731d5f9d3a566e
Li Han Lin <119099121+hanlinnnnn@users.noreply.github.com>
2024-05-29 13:02:47 +0800

    Update and rename datasetnice.py to parsedata.py

--- parsedata.py
+++ parsedata.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Specify the path to your CSV file
-csv_file_path = '/Users/lihanlin/Downloads/2022-2023 Football Team Stats.csv'
+csv_file_path = 'path/to/your data.csv'
 
 # Read the CSV file with the correct delimiter
 df = pd.read_csv(csv_file_path, encoding='ISO-8859-1', delimiter=';')
@@ -12,4 +12,4 @@ df.index = range(1, len(df) + 1)
 print(df.head())
 
 # Optional: Save the correctly parsed DataFrame to a new CSV file
-df.to_csv('/Users/lihanlin/Downloads/Parsed_Football_Team_Stats_2022-2023.csv', index=True)
+df.to_csv('path/to/new data.csv', index=True)
