41909631472069e0d070d1851ed094ce8c07032b
Li Han Lin <119099121+hanlinnnnn@users.noreply.github.com>
2024-05-30 15:35:33 +0800

    Update parsedata.py

--- parsedata.py
+++ parsedata.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Specify the path to your CSV file
-csv_file_path = '/Users/lihanlin/Downloads/2022-2023 Football Player Stats.csv'
+csv_file_path = '/path/to/your_csv.csv'
 
 # Read the CSV file with the correct delimiter
 df = pd.read_csv(csv_file_path, encoding='ISO-8859-1', delimiter=';')
@@ -9,5 +9,5 @@ df = pd.read_csv(csv_file_path, encoding='ISO-8859-1', delimiter=';')
 # Display the DataFrame to check if it's correctly parsed
 print(df.head(50))
 
-# Optional: Save the correctly parsed DataFrame to a new CSV file
-df.to_excel('/Users/lihanlin/Downloads/Parsed_Football_Player_Stats_2022-2023.xlsx', index=False)
+# Optional: Save the correctly parsed DataFrame to a new excel file
+df.to_excel('/path/to/your_xlsx.xlsx', index=False)
